e4f8b3e0f342a157d1b9ac1ab4c97c22489cf315
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-29 10:32:11 -0700

    Configure morgan

--- middleware/config.js
+++ middleware/config.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 module.exports = server => {
 	server.use(express.json());
 	server.use(bodyParser.json());
-	server.use(morgan());
+	server.use(morgan("combined"));
 	server.use(helmet());
 	server.use(cors());
 };
