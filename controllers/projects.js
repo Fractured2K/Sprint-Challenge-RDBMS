69b2e9b4e5625cd120c8671075fea3b3c42325f1
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-29 09:01:54 -0700

    Handle get projects endpoint

--- controllers/projects.js
+++ controllers/projects.js
@@ -4,11 +4,9 @@ const Projects = require("../models/Projects");
 // Create project
 router.post("/", async (req, res) => {
 	try {
-		console.log(req.body);
 		const project = await Projects.addProject(req.body);
 		res.status(200).json(project);
 	} catch (err) {
-		console.log(err);
 		res.status(500).json({
 			message: "Sorry, but there was an error while creating the project"
 		});
@@ -16,6 +14,15 @@ router.post("/", async (req, res) => {
 });
 
 // Get all projects
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+	try {
+		const projects = await Projects.find();
+		res.status(200).json(projects);
+	} catch (err) {
+		res.status(500).json({
+			message: "Sorry, but we couldn't get any projects"
+		});
+	}
+});
 
 module.exports = router;
