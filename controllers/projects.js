a3aaea1cc2375177591dc5c46c9b974465d59c0d
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-29 09:04:01 -0700

    Handle get project by id endpoint

--- controllers/projects.js
+++ controllers/projects.js
@@ -5,7 +5,7 @@ const Projects = require("../models/Projects");
 router.post("/", async (req, res) => {
 	try {
 		const project = await Projects.addProject(req.body);
-		res.status(200).json(project);
+		res.status(201).json(project);
 	} catch (err) {
 		res.status(500).json({
 			message: "Sorry, but there was an error while creating the project"
@@ -26,6 +26,15 @@ router.get("/", async (req, res) => {
 });
 
 // Get project by id
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+	try {
+		const project = await Projects.findById(req.params.id);
+		res.status(200).json(project);
+	} catch (err) {
+		res.status(500).json({
+			message: "Sorry, but we couldn't get that project"
+		});
+	}
+});
 
 module.exports = router;
