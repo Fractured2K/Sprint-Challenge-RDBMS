6dcfa33608d0354e7dc5ea250aa8c01a69dd3748
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-29 08:52:26 -0700

    Define create project endpoint

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,3 +1,7 @@
 const router = require("express").Router();
+const Project = require("../models/Projects");
+
+// Create project
+router.post("/", async (req, res) => {});
 
 module.exports = router;
