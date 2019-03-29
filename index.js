3269ef4c9430e845e997a20c4d60cb1b6be5ecae
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-29 08:46:39 -0700

    Implement server middleware

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const server = express();
+
+server.use(express.json());
+server.use(bodyParser.json());
+server.use(morgan());
+server.use(helmet());
+server.use(cors());
 
 server.listen(5000, () =>
 	console.log("Server now running on http://localhost:5000")
